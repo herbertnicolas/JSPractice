2b5301364d04bf212f84b55e03f3edb58bf23085
herbertnicolas <hna@cin.ufpe.br>
2022-04-03 16:40:24 -0300

    evento usando DOMContentLoaded

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function sayHello(event){
-    var name = document.getElementById("name").value;
-    var msg = "<h2>Hello " + name + "!</h2>"
-    document.getElementById("content").innerHTML = msg;
-
-    if(name === "Herbert"){
-        var title = document.querySelector("#title").textContent;
-        title += " & lovin it!";
-        document.querySelector("#title").textContent = title;
+//adicionando um evento em que pegando o conteudo carregado
+//sem analisar o css, imagens e etc e executa as funções
+//obs: pra que funcione o script deve ser carregado no head do html
+document.addEventListener("DOMContentLoaded", 
+function (event){ 
+    function sayHello(event){
+        this.textContent = "Said it!";
+        var name = document.getElementById("name").value;
+        var msg = "<h2>Hello " + name + "!</h2>"
+        document.getElementById("content").innerHTML = msg;
+    
+        if(name === "Herbert"){
+            var title = document.querySelector("#title").textContent;
+            title += " & lovin it!";
+            document.querySelector("h1").textContent = title;
+        }
     }
-}
+    document.querySelector("button").addEventListener("click", sayHello);
+})
+
