b5bf98680d9993f9462cf0c9ba04e4d1de2ed0bf
herbertnicolas <hna@cin.ufpe.br>
2022-04-03 03:13:02 -0300

    Lecture 54 practice

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function sayHello(onClick){
+function sayHello(event){
     var name = document.getElementById("name").value;
-    var message = "<h2>Hello " + name + "!<\h2>";
-    document.getElementById("content").innerHTML = message;
+    var msg = "<h2>Hello " + name + "!</h2>"
+    document.getElementById("content").innerHTML = msg;
+
+    if(name === "Herbert"){
+        var title = document.querySelector("#title").textContent;
+        title += " & lovin it!";
+        document.querySelector("#title").textContent = title;
+    }
 }
